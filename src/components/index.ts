56542e639a5e3d707f22ad576a030957770e28db
usaeric2025-star <usaeric2025@gmail.com>
2026-05-20 16:30:29 +0800

    refactor: Remove unused ErrorAlert component
    The ErrorAlert component is no longer used in the application and has been removed to clean up the codebase.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export * from './photo';
-export { ErrorAlert } from './ErrorAlert';
 export { ErrorBoundary } from './ErrorBoundary';
 export { SettingsScreen } from './SettingsScreen';
 export { StaffUnlockDialog } from './StaffUnlockDialog';
