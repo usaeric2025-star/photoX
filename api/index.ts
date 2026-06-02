b9396d26f240d26aa515164451bfa1ea7b2b0fa3
usaeric2025-star <usaeric2025@gmail.com>
2026-06-02 18:41:21 +0800

    hi

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,5 @@
 import { handle } from "hono/vercel";
-
-let app;
-try {
-  const module = await import("./app");
-  app = module.app;
-  console.log("[API] Server module loaded successfully");
-} catch (err) {
-  console.error("[API] Failed to load server module:", err);
-  throw err;
-}
+import { app } from "./app.js";
 
 export default handle(app);
+
