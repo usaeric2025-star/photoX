bfc3c835c69129ed5e5643f7d10ab93839284ee5
usaeric2025-star <usaeric2025@gmail.com>
2026-06-18 00:45:43 +0800

    hi

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import * as schema from './schema';
-import { getServerEnv } from '../shared/envSchema';
+import * as schema from './schema.js';
+import { getServerEnv } from '../shared/envSchema.js';
 
 const env = getServerEnv(process.env);
 
