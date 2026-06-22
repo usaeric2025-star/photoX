1aafccb397c3abc3d3334ed62a540867810a4f93
usaeric2025-star <usaeric2025@gmail.com>
2026-06-23 02:17:08 +0800

    hi

--- src/features/diagnostics/index.ts
+++ src/features/diagnostics/index.ts
@@ -1,9 +1,3 @@
 export { DiagnosticsDashboard } from './DiagnosticsDashboard';
-export { MaintenanceCenter } from './MaintenanceCenter';
-export { IssueList } from './IssueList';
-export { AuditVisualizer } from './AuditVisualizer';
-export { DiagnosticCard } from './DiagnosticCard';
-export { TasksContent as TasksList } from './TasksList';
-export { ErrorLogViewer } from './ErrorLogViewer';
 export * from './registry';
 export * from './issueActions';
