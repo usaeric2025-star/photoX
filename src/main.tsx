1a2486d0e17c2862d54ad45c83df0d8b9f31df78
usaeric2025-star <usaeric2025@gmail.com>
2026-06-28 00:30:59 +0800

    hi

--- src/main.tsx
+++ src/main.tsx
@@ -30,6 +30,14 @@ if (typeof window !== 'undefined') {
 }
 
 
+import { scan } from 'react-scan';
+
+if (typeof window !== 'undefined') {
+  scan({
+    enabled: true,
+  });
+}
+
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { SWRConfig } from 'swr';
