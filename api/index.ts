3bb87f5b6e264e9e390456be4648b739d59e9ecf
usaeric2025-star <usaeric2025@gmail.com>
2026-06-30 05:55:49 +0800

    hi

--- api/index.ts
+++ api/index.ts
@@ -5,4 +5,5 @@ export const GET = handle(app);
 export const POST = handle(app);
 export const PUT = handle(app);
 export const DELETE = handle(app);
+export const PATCH = handle(app);
 export const OPTIONS = handle(app);
